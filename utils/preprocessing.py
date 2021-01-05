c35f59d8a7ec6d0839a5048bb0136a4dc24c10a4
forallx94 <forallx94@gmail.com>
2021-01-05 16:15:09 +0900

    first commit

---
+++ utils/preprocessing.py
@@ -0,0 +1,29 @@
+"""
+The interface for data preprocessing.
+
+Authors:
+    LogPAI Team
+
+"""
+
+
+import pandas as pd
+import numpy as np
+
+class Vectorizer(object):
+
+    def fit_transform(self, x_train, window_y_train, y_train):
+        self.label_mapping = {eid: idx for idx, eid in enumerate(window_y_train.unique(), 2)}
+        self.label_mapping["#OOV"] = 0
+        self.label_mapping["#Pad"] = 1
+        self.num_labels = len(self.label_mapping)
+        return self.transform(x_train, window_y_train, y_train)
+
+    def transform(self, x, window_y, y):
+        new_x = x.copy()
+        new_x["EventSequence"] = new_x["EventSequence"].map(lambda x: [self.label_mapping.get(item, 0) for item in x])
+        window_y = window_y.map(lambda x: self.label_mapping.get(x, 0))
+        y = y
+        data_dict = {"SessionId": new_x["SessionId"].values, "window_y": window_y.values, "y": y.values, "x": np.array(new_x["EventSequence"].tolist())}
+        return data_dict
+        
